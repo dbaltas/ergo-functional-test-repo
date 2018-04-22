3a856894b744ddbfe69bead5a1db8fec8ef06200
Dimitris Baltas <dimitris.baltas@thebeat.co>
2018-04-22 16:07:50 +0300

    commands to create/remove test branches and run ergo

---
+++ ergo-list.sh
@@ -0,0 +1,1 @@
+ergo -directory . -branches 'ft-0001-develop,ft-0001-master,ft-0001-release-gr,ft-0001-release-it,ft-0001-release-es' -baseBranch ft-0001-master
---
+++ remote-branches-remove.sh
@@ -0,0 +1,5 @@
+git push origin :ft-0001-develop
+git push origin :ft-0001-master
+git push origin :ft-0001-release-gr
+git push origin :ft-0001-release-it
+git push origin :ft-0001-release-es
